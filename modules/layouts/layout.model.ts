45a48b3fd17c97db34a18cea932de1ce4fb9ac6b
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-20 17:27:42 +0600

    Add mongoose schema for layouts 🛠️

---
+++ modules/layouts/layout.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import { LayoutModel, TLayout } from "./layout.interface";
+
+const LayoutSchema = new Schema<TLayout, Record<string, unknown>>({
+  type: {
+    type: String,
+    enum: ["banner", "faq", "categories"],
+    unique: true,
+  },
+  faq: {
+    type: [
+      {
+        question: {
+          type: String,
+        },
+        answer: {
+          type: String,
+        },
+      },
+    ],
+    default: undefined,
+  },
+  banner: {
+    type: {
+      title: {
+        type: String,
+      },
+      subTitle: {
+        type: String,
+      },
+    },
+    default: undefined,
+  },
+  categories: {
+    type: [
+      {
+        categoryTitle: {
+          type: String,
+        },
+      },
+    ],
+    default: undefined,
+  },
+});
+
+export const Layout = model<TLayout, LayoutModel>("Layout", LayoutSchema);

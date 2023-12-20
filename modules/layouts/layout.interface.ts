9cbfacbd7dac16c45014408cebe4c2830f9bba73
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-20 17:26:35 +0600

    Add types for banner, layout, and data structures 🚀

---
+++ modules/layouts/layout.interface.ts
@@ -0,0 +1,36 @@
+import { Model } from "mongoose";
+
+export type TBanner = {
+  image: {
+    publicId: string;
+    url: string;
+  };
+  title: string;
+  subTitle: string;
+};
+
+// model interfaca
+export type TLayout = {
+  faq: {
+    question: string;
+    answer: string;
+  }[];
+  banner: TBanner;
+
+  categories: {
+    categoryTitle: string;
+  }[];
+
+  type: "banner" | "faq" | "categories";
+};
+
+// payload
+export type TData = {
+  type: string;
+  bannerTitle: string;
+  bannerSubTitle: string;
+  categories: [];
+  faq: [];
+};
+
+export type LayoutModel = Model<TLayout, Record<string, unknown>>;

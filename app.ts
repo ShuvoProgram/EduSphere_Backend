205b49fdf9e7941d10d8cd050a63b0070448f665
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-22 19:06:52 +0600

    added client side api

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://edu-sphere-frontend.vercel.app/",
     credentials: true,
   })
 );

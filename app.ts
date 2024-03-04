ae1c6b736d783a050a517f0f41fc7b3501993c9d
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-04 19:23:31 +0600

    fix issued

--- app.ts
+++ app.ts
@@ -15,7 +15,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://edu-sphere-frontend.vercel.app", "http://localhost:3000"],
+    // origin: ["https://edu-sphere-frontend.vercel.app", "http://localhost:3000"],
+    origin: "https://edu-sphere-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
   })

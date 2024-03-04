fb61b874ceda93a25fbab4de480cc9f403e478b9
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-04 17:35:30 +0600

    error

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: ["https://edu-sphere-frontend.vercel.app", "http://localhost:3000"],
     credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
   })
 );
 
